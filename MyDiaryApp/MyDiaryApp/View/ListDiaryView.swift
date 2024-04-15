020ec5497bf42e3f113852385c7cb6c1ae87e1d7
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 22:03:25 +0900

    Make diary examples

--- MyDiaryApp/MyDiaryApp/View/ListDiaryView.swift
+++ MyDiaryApp/MyDiaryApp/View/ListDiaryView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 struct ListDiaryView: View {
     var body: some View {
         ZStack {
-            Text("Text List View")
+            ScrollView {
+                VStack {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("건.뜨.")
+                                .font(.title2)
+                            Spacer()
+                            Text("2024년 4월 11일")
+                                .font(.subheadline)
+                            Text("🧶")
+                                .font(.subheadline)
+                        }
+                        .padding(.bottom, 10)
+                        Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                        Divider()
+                    }
+                    .padding(.vertical, 10)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("건.수.")
+                                .font(.title2)
+                            Spacer()
+                            Text("2024년 4월 10일")
+                                .font(.subheadline)
+                            Text("🌊")
+                                .font(.subheadline)
+                        }
+                        .padding(.bottom, 10)
+                        Text("매일 수영을 하고 싶다. 내일도 수영 해야지. 매일 수영을 하고 싶다. 내일도 수영 해야지. 매일 수영을 하고 싶다. 내일도 수영 해야지...")
+                        Divider()
+                    }
+                    .padding(.vertical, 10)
+                }
+            }
+            .padding()
             FixedView(isCalendarView: false)
         }
     }
