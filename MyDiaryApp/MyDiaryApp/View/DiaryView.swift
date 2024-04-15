2e15519be1087ef295e30f2fe7334dbee324b3a5
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-16 08:35:37 +0900

    Make save button in DiaryView

--- MyDiaryApp/MyDiaryApp/View/DiaryView.swift
+++ MyDiaryApp/MyDiaryApp/View/DiaryView.swift
@@ -9,24 +9,41 @@ import SwiftUI
 
 struct DiaryView: View {
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading, spacing: 20) {
-                HStack {
-                    Text("2024년 4월 11일")
-                    Spacer()
-                    Text("🧶")
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    HStack {
+                        Text("2024년 4월 11일")
+                        Spacer()
+                        Text("🧶")
+                    }
+                    Text("건.뜨.")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    Divider()
+                    Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                    Divider()
+                    Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                    Divider()
+                    
                 }
-                Text("건.뜨.")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Divider()
-                Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
-                Divider()
-                Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
-                Divider()
-                
             }
+            .padding()
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("save new diary clicked")
+                    }, label: {
+                        Image(systemName: "checkmark.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    })
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
