9e4a1e99730bc43a9c6f685e5dfc717a27a5040e
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 23:06:40 +0900

    Make DiaryView

---
+++ MyDiaryApp/MyDiaryApp/View/DiaryView.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryView.swift
+//  MyDiaryApp
+//
+//  Created by Groo on 4/15/24.
+//
+
+import SwiftUI
+
+struct DiaryView: View {
+    var body: some View {
+        Text("Diary View")
+    }
+}
+
+#Preview {
+    DiaryView()
+}
