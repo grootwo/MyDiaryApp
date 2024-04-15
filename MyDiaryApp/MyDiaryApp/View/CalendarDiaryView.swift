9065b483e0163ed74f541a982f537637d5baa34e
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 14:44:05 +0900

    Make main View files and regroup

---
+++ MyDiaryApp/MyDiaryApp/View/CalendarDiaryView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarDiaryView.swift
+//  MyDiaryApp
+//
+//  Created by Groo on 4/15/24.
+//
+
+import SwiftUI
+
+struct CalendarDiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarDiaryView()
+}
---
+++ MyDiaryApp/MyDiaryApp/View/ListDiaryView.swift
@@ -0,0 +1,18 @@
+//
+//  ListDiaryView.swift
+//  MyDiaryApp
+//
+//  Created by Groo on 4/15/24.
+//
+
+import SwiftUI
+
+struct ListDiaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListDiaryView()
+}
