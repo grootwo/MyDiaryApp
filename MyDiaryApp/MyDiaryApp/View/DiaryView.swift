6603bc8f077a7f9d8d598e1aa5d007c7de1a4460
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 23:13:49 +0900

    Show contents in DiaryView

--- MyDiaryApp/MyDiaryApp/View/DiaryView.swift
+++ MyDiaryApp/MyDiaryApp/View/DiaryView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct DiaryView: View {
     var body: some View {
-        Text("Diary View")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text("2024년 4월 11일")
+                    Spacer()
+                    Text("🧶")
+                }
+                Text("건.뜨.")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Divider()
+                Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                Divider()
+                Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                Divider()
+                
+            }
+        }
+        .padding()
     }
 }
 
