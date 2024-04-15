493b17ca0b008c6873f36fcdbbc9160ea9397f4d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 12:26:47 +0900

    Make MyDiaryApp project

---
+++ MyDiaryApp/MyDiaryApp/MyDiaryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryAppApp.swift
+//  MyDiaryApp
+//
+//  Created by Groo on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
