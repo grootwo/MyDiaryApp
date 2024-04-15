18f28d6734c72f5d8f59982fc2f798a7fc35092d
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-16 08:45:08 +0900

    Copy and paste more diary examples

--- MyDiaryApp/MyDiaryApp/View/ListDiaryView.swift
+++ MyDiaryApp/MyDiaryApp/View/ListDiaryView.swift
@@ -42,6 +42,37 @@ struct ListDiaryView: View {
                         Divider()
                     }
                     .padding(.vertical, 10)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("건.뜨.")
+                                .font(.title2)
+                            Spacer()
+                            Text("2024년 4월 11일")
+                                .font(.subheadline)
+                            Text("🧶")
+                                .font(.subheadline)
+                        }
+                        .padding(.bottom, 10)
+                        Text("매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지. 매일 뜨개질을 하고 싶다. 내일도 뜨개질 해야지...")
+                        Divider()
+                    }
+                    .padding(.vertical, 10)
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("건.수.")
+                                .font(.title2)
+                            Spacer()
+                            Text("2024년 4월 10일")
+                                .font(.subheadline)
+                            Text("🌊")
+                                .font(.subheadline)
+                        }
+                        .padding(.bottom, 10)
+                        Text("매일 수영을 하고 싶다. 내일도 수영 해야지. 매일 수영을 하고 싶다. 내일도 수영 해야지. 매일 수영을 하고 싶다. 내일도 수영 해야지...")
+                        Divider()
+                    }
+                    .padding(.vertical, 10)
+
                 }
             }
             .padding()
