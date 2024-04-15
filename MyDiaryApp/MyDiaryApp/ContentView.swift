cfd1de73aeed597f1e7b3240ac97caaba07e2126
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-15 12:51:00 +0900

    Show DatePicker and Button to make new diary

--- MyDiaryApp/MyDiaryApp/ContentView.swift
+++ MyDiaryApp/MyDiaryApp/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var selectedDay = Date()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            DatePicker("", selection: $selectedDay, displayedComponents: [.date])
+                .datePickerStyle(.graphical)
+            VStack {
+                HStack {
+                    TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                }
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("Click new diary button")
+                    }, label: {
+                        Image(systemName: "pencil.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    })
+
+                }
+            }
         }
         .padding()
     }
