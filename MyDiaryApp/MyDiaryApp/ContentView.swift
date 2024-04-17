b82cfa400abc6d0a80d19bc000769d5afdb05122
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-17 21:33:05 +0900

    Set DateFormatter

--- MyDiaryApp/MyDiaryApp/ContentView.swift
+++ MyDiaryApp/MyDiaryApp/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isCalendarView = true
+    static let dateFormat: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY/M/d"
+        return formatter
+    }()
     var body: some View {
         NavigationStack {
             ZStack {
